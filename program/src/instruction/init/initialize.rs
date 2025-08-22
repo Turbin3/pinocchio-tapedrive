4f1ccdbfd9970b077c34727da5eae0bd719bc60a
moses7054 <moses7054@gmail.com>
2025-08-22 18:14:45 +0530

    adding initialize function

--- program/src/instruction/init/initialize.rs
+++ program/src/instruction/init/initialize.rs
@@ -6,6 +6,76 @@ use pinocchio::{
     ProgramResult,
 };
 
+trait AccountInfoExt {
+    fn check_account(&self, seed: &[u8]) -> ProgramResult;
+    fn check_account_with_address(&self, address: &Pubkey) -> ProgramResult;
+    fn is_program_check(&self) -> ProgramResult;
+}
+
+impl AccountInfoExt for AccountInfo {
+    fn check_account(&self, seed: &[u8]) -> ProgramResult {
+        if !self.data_is_empty() {
+            return Err(ProgramError::AccountAlreadyInitialized);
+        }
+        if !self.is_writable() {
+            return Err(ProgramError::Immutable);
+        }
+        let (pda, _bump) = find_program_address(&[seed], &TAPE_ID);
+
+        if self.key() != &pda {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        Ok(())
+    }
+
+    fn check_account_with_address(&self, address: &Pubkey) -> ProgramResult {
+        if !self.data_is_empty() {
+            return Err(ProgramError::AccountAlreadyInitialized);
+        }
+        if !self.is_writable() {
+            return Err(ProgramError::Immutable);
+        }
+
+        if self.key().ne(address) {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        Ok(())
+    }
+
+    fn is_program_check(&self) -> ProgramResult {
+        if self.key().ne(&TAPE_ID) {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        if !self.executable() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        Ok(())
+    }
+}
+
+enum GetPda {
+    Metadata(Pubkey),
+    Mint,
+    Treasury,
+}
+
+impl GetPda {
+    pub fn address(&self) -> (Pubkey, u8) {
+        match self {
+            GetPda::Mint => {
+                find_program_address(&[b"mint", &[152, 68, 212, 200, 25, 113, 221, 71]], &TAPE_ID)
+            }
+            GetPda::Treasury => find_program_address(&[b"treasury"], &TAPE_ID),
+            GetPda::Metadata(mint) => find_program_address(
+                &[b"metadata", MPL_TOKEN_METADATA_ID.as_ref(), mint.as_ref()],
+                &MPL_TOKEN_METADATA_ID,
+            ),
+        }
+    }
+}
+
 pub fn process_initialize(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
     if !data.is_empty() {
         return Err(ProgramError::InvalidInstructionData);
@@ -17,19 +87,20 @@ pub fn process_initialize(accounts: &[AccountInfo], data: &[u8]) -> ProgramResul
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
-    check_account(&archive_info, ARCHIVE)?;
-    check_account(&epoch_info, EPOCH)?;
-    check_account(&block_info, BLOCK)?;
-    let (mint_address, mint_bump) = get_pda(GetPda::Mint);
-    let (treasury_address, treasury_bump) = get_pda(GetPda::Treasury);
-    let (metadata_address, _metadata_bump) = get_pda(GetPda::Metadata(mint_address));
+    archive_info.check_account(ARCHIVE)?;
+    epoch_info.check_account(EPOCH)?;
+    block_info.check_account(BLOCK)?;
+
+    let (mint_address, mint_bump) = GetPda::Mint.address();
+    let (treasury_address, treasury_bump) = GetPda::Treasury.address();
+    let (metadata_address, _metadata_bump) = GetPda::Metadata(mint_address).address();
 
     assert_eq!(mint_bump, MINT_BUMP);
     assert_eq!(treasury_bump, TREASURY_BUMP);
 
-    check_account_with_address(&mint_info, &mint_address)?;
-    check_account_with_address(&metadata_info, &metadata_address)?;
-    check_account_with_address(&treasury_info, &treasury_address)?;
+    mint_info.check_account_with_address(&mint_address)?;
+    metadata_info.check_account_with_address(&metadata_address)?;
+    treasury_info.check_account_with_address(&treasury_address)?;
 
     if !treasury_ata_info.data_is_empty() {
         return Err(ProgramError::AccountAlreadyInitialized);
@@ -38,73 +109,13 @@ pub fn process_initialize(accounts: &[AccountInfo], data: &[u8]) -> ProgramResul
         return Err(ProgramError::Immutable);
     }
 
-    is_program_check(&tape_program_info)?;
-    is_program_check(&system_program_info)?;
-    is_program_check(&token_program_info)?;
-    is_program_check(&associated_token_program_info)?;
-    is_program_check(&metadata_program_info)?;
-    is_program_check(&rent_sysvar_info)?;
-    is_program_check(&slot_hashes_info)?;
-
-    Ok(())
-}
-
-fn check_account(account: &AccountInfo, seed: &[u8]) -> Result<(), ProgramError> {
-    if !account.data_is_empty() {
-        return Err(ProgramError::AccountAlreadyInitialized);
-    }
-    if !account.is_writable() {
-        return Err(ProgramError::Immutable);
-    }
-    let (pda, _bump) = find_program_address(&[seed], &TAPE_ID);
-
-    if account.key() != &pda {
-        return Err(ProgramError::InvalidAccountData);
-    }
-    Ok(())
-}
-
-fn get_pda(get_pda: GetPda) -> (Pubkey, u8) {
-    match get_pda {
-        GetPda::Mint => {
-            find_program_address(&[b"mint", &[152, 68, 212, 200, 25, 113, 221, 71]], &TAPE_ID)
-        }
-        GetPda::Treasury => find_program_address(&[b"treasury"], &TAPE_ID),
-        GetPda::Metadata(mint) => find_program_address(
-            &[b"metadata", MPL_TOKEN_METADATA_ID.as_ref(), mint.as_ref()],
-            &MPL_TOKEN_METADATA_ID,
-        ),
-    }
-}
-
-enum GetPda {
-    Metadata(Pubkey),
-    Mint,
-    Treasury,
-}
-
-fn check_account_with_address(account: &AccountInfo, address: &Pubkey) -> Result<(), ProgramError> {
-    if !account.data_is_empty() {
-        return Err(ProgramError::AccountAlreadyInitialized);
-    }
-    if !account.is_writable() {
-        return Err(ProgramError::Immutable);
-    }
-
-    if account.key().ne(address) {
-        return Err(ProgramError::InvalidAccountData);
-    }
-    Ok(())
-}
-
-fn is_program_check(account: &AccountInfo) -> Result<(), ProgramError> {
-    if account.key().ne(&TAPE_ID) {
-        return Err(ProgramError::InvalidAccountData);
-    }
-
-    if !account.executable() {
-        return Err(ProgramError::InvalidAccountData);
-    }
+    tape_program_info.is_program_check()?;
+    system_program_info.is_program_check()?;
+    token_program_info.is_program_check()?;
+    associated_token_program_info.is_program_check()?;
+    metadata_program_info.is_program_check()?;
+    rent_sysvar_info.is_program_check()?;
+    slot_hashes_info.is_program_check()?;
 
     Ok(())
 }
