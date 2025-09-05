a5177dac541fdcb74258785d6aeb930f947b7373
moses7054 <moses7054@gmail.com>
2025-09-05 17:29:19 +0530

    removed solana dependencies

---
+++ api/src/state/archive.rs
@@ -0,0 +1,33 @@
+use super::AccountType;
+use crate::state::utils::{load_acc, load_acc_mut, DataLen, Initialized};
+use bytemuck::{Pod, Zeroable};
+use pinocchio::program_error::ProgramError;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
+pub struct Archive {
+    pub tapes_stored: u64,
+    pub segments_stored: u64,
+}
+
+impl DataLen for Archive {
+    const LEN: usize = core::mem::size_of::<Archive>();
+}
+
+impl Initialized for Archive {
+    fn is_initialized(&self) -> bool {
+        true
+    }
+}
+
+impl Archive {
+    pub fn unpack(data: &[u8]) -> Result<&Self, ProgramError> {
+        unsafe { load_acc::<Archive>(data) }
+    }
+
+    pub fn unpack_mut(data: &mut [u8]) -> Result<&mut Self, ProgramError> {
+        unsafe { load_acc_mut::<Archive>(data) }
+    }
+}
+
+// account!(AccountType, Archive);
---
+++ api/src/state/treasury.rs
@@ -0,0 +1,29 @@
+use super::AccountType;
+use crate::state::utils::{load_acc, load_acc_mut, DataLen, Initialized};
+use bytemuck::{Pod, Zeroable};
+use pinocchio::program_error::ProgramError;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
+pub struct Treasury {}
+
+impl DataLen for Treasury {
+    const LEN: usize = core::mem::size_of::<Treasury>();
+}
+
+impl Initialized for Treasury {
+    fn is_initialized(&self) -> bool {
+        true
+    }
+}
+
+impl Treasury {
+    pub fn unpack(data: &[u8]) -> Result<&Self, ProgramError> {
+        unsafe { load_acc::<Treasury>(data) }
+    }
+    pub fn unpack_mut(data: &mut [u8]) -> Result<&mut Self, ProgramError> {
+        unsafe { load_acc_mut::<Treasury>(data) }
+    }
+}
+
+// account!(AccountType, Treasury);
