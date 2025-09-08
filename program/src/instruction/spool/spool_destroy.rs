53f7c19a06efda98a11a2c68cc9fa467f8550b14
Darren <darren26feb@gmail.com>
2025-09-09 00:46:39 +0530

    spool_destroy fn

--- program/src/instruction/spool/spool_destroy.rs
+++ program/src/instruction/spool/spool_destroy.rs
@@ -1,5 +1,29 @@
+use crate::api::state::Spool;
 use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
 
-pub fn process_spool_destroy(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+pub fn process_spool_destroy(accounts: &[AccountInfo], _data: &[u8]) -> ProgramResult {
+    let [signer_info, spool_info, _system_program_info, _remaining @ ..] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    if !signer_info.is_signer() {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if !spool_info.is_writable() {
+        return Err(ProgramError::Immutable);
+    }
+
+    let spool_data = spool_info.try_borrow_mut_data()?;
+    let spool = Spool::unpack(&spool_data)?;
+
+    if spool.authority != *signer_info.key() {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    *signer_info.try_borrow_mut_lamports()? += *spool_info.try_borrow_lamports()?;
+    *spool_info.try_borrow_mut_lamports()? = 0;
+    spool_info.close()?;
+
     Ok(())
 }
