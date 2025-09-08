6cb6261b3db1fa8280d7f88a592ed97cf23aa382
Darren <darren26feb@gmail.com>
2025-09-08 23:21:41 +0530

    spool_create fn

--- program/src/instruction/spool/spool_create.rs
+++ program/src/instruction/spool/spool_create.rs
@@ -1,5 +1,93 @@
-use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
+use crate::api::prelude::*;
+use crate::api::state::utils::DataLen as ApiDataLen;
+use crate::api::state::Spool;
+use pinocchio::{
+    account_info::AccountInfo,
+    instruction::{Seed, Signer},
+    program_error::ProgramError,
+    sysvars::{clock::Clock, rent::Rent, Sysvar},
+    ProgramResult,
+};
+use pinocchio_system::instructions::CreateAccount;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, shank::ShankType)]
+pub struct CreateSpoolIxData {
+    pub number: u64,
+}
+
+impl DataLen for CreateSpoolIxData {
+    const LEN: usize = core::mem::size_of::<CreateSpoolIxData>();
+}
 
 pub fn process_spool_create(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let current_time = Clock::get()?.unix_timestamp;
+    let [signer_info, miner_info, spool_info, _system_program_info, rent_info, _remaining @ ..] =
+        accounts
+    else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    if !signer_info.is_signer() {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if !spool_info.data_is_empty() {
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    if !spool_info.is_writable() {
+        return Err(ProgramError::Immutable);
+    }
+
+    let miner_data = miner_info.try_borrow_data()?;
+    let miner = Miner::unpack(&miner_data)?;
+
+    if miner.authority != *signer_info.key() {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let ix_data = unsafe { load_ix_data::<CreateSpoolIxData>(&data)? };
+
+    let spool_number = ix_data.number;
+    let (spool_pda, spool_bump) = spool_pda(*miner_info.key(), spool_number);
+
+    if spool_pda.ne(spool_info.key()) {
+        return Err(ProgramError::InvalidAccountOwner);
+    }
+
+    let rent = Rent::from_account_info(rent_info)?;
+
+    let bump_binding = [spool_bump];
+    let spool_number_bytes = spool_number.to_le_bytes();
+    let signer_seeds = [
+        Seed::from(SPOOL),
+        Seed::from(miner_info.key().as_ref()),
+        Seed::from(&spool_number_bytes),
+        Seed::from(&bump_binding),
+    ];
+    let signers = [Signer::from(&signer_seeds[..])];
+
+    CreateAccount {
+        from: signer_info,
+        to: spool_info,
+        space: <Spool as ApiDataLen>::LEN as u64,
+        owner: &crate::ID,
+        lamports: rent.minimum_balance(<Spool as ApiDataLen>::LEN),
+    }
+    .invoke_signed(&signers)?;
+
+    let mut spool_data = spool_info.try_borrow_mut_data()?;
+    let spool = Spool::unpack_mut(&mut spool_data)?;
+
+    spool.number = spool_number;
+    spool.authority = *signer_info.key();
+    spool.last_proof_at = current_time;
+    spool.last_proof_block = 0;
+    // spool.seed =
+    // spool.state = TapeTree::new(&[spool_info.key.as_ref()]);
+    spool.contains = [0; 32];
+    spool.total_tapes = 0;
+
     Ok(())
 }
